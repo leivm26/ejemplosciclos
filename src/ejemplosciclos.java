324769afd08b48dc3a8c488974940892bc4117dc
andrey rojas <levamo26@outlook.com>
2022-06-29 15:58:00 -0500

    cambios

---
+++ src/ejemplosciclos.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class ejemplosciclos {
+    public static void main (String [] args){
+        for (int i = 0; i < 3; i++){
+            JOptionPane.showInputDialog(null,"hola mundo");
+        }
+    }
+}
+
---
+++ src/numeros_131313.java
@@ -0,0 +1,16 @@
+public class numeros_131313 {
+    public static void main (String [] args) {
+        //imprimir 10 numeros
+        for (int i = 1; i <= 6; i++) {
+            int E;
+            if (i % 2 == 0) {
+                E = 3;
+            } else {
+                    E = 1;
+                    System.out.println("numero: " + E);
+
+                }
+            }
+        }
+    }
+
